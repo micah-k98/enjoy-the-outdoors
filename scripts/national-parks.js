ba7bb76853919aadc1ed5ee823810e0bae24fddb
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-14 09:19:52 -0700

    Added scripts src in national-parks html, and added functions in national-parks js

---
+++ scripts/national-parks.js
@@ -0,0 +1,14 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    displayParks(nationalParksArray);
+})
+
+function displayParks(parks) {
+    const mainContainer = document.getElementById("display-parks-content");
+    mainContainer.innerText = "";
+
+
+}
+
