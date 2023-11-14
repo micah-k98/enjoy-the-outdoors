f4ecec41a24698d0642511af025a8e3fa30caca4
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-14 12:43:58 -0700

    Added functions to display all details in national-park page

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -9,9 +9,9 @@ function displayParks(parks) {
     const mainContainer = document.getElementById("display-parks-content");
     mainContainer.innerText = "";
 
-    // parks.forEach(park => {
-        displayPark(parks[0], mainContainer);
-    // });
+    parks.forEach(park => {
+        displayPark(park, mainContainer);
+    });
 }
 
 function displayPark(park, parentContainer) {
@@ -23,23 +23,55 @@ function displayPark(park, parentContainer) {
 }
 
 function displayDetails(park, parentDiv) {
-    const parkNameH4 = document.createElement("h4");
-    const addressP = document.createElement("p");
-    const phoneNumberP = document.createElement("p");
-    const faxNumberP = document.createElement("p");
-    const visitA = document.createElement("a");
+    let details = {};
+    details.parkNameH4 = document.createElement("h4");
+    details.addressP = document.createElement("p");
+    details.phoneNumberP = document.createElement("p");
+    details.faxNumberP = document.createElement("p");
+    details.visitP = document.createElement("p");
+        details.visitA = document.createElement("a");
+        details.visitP.appendChild(details.visitA);
 
-    parkNameH4.innerText = park.LocationName;
-    addressP.innerText = `Address: ${park.Address}, ${park.City}, ${park.State} ${park.ZipCode}`;
-    phoneNumberP.innerText = park.Phone;
-    faxNumberP.innerText = park.Fax;
-    visitA.innerText = park.Visit;
-    visitA.href = park.Visit;
-    visitA.target = "_blank"
+    details.parkNameH4.innerText = park.LocationName;
+    parentDiv.appendChild(details.parkNameH4);
+    
+    doesValueExists(details, park, parentDiv);
+    
+    
+    
+    
+    
+    
+}
 
-    parentDiv.appendChild(parkNameH4);
-    parentDiv.appendChild(addressP);
-    parentDiv.appendChild(phoneNumberP);
-    parentDiv.appendChild(faxNumberP);
-    parentDiv.appendChild(visitA);
+function doesValueExists(details, park, parentDiv) {
+    let parkAddress = {};
+    details.addressP.innerText = "Address: ";
+    for(const key in park) {
+        if(key == "Address" || key == "City")
+        {
+            if(park[key] != 0) details.addressP.innerText += `${park[key]}, `;
+        }
+        else if (key == "State" || key == "ZipCode") details.addressP.innerText += `${park[key]} `;
+    }
+    parentDiv.appendChild(details.addressP);
+    
+    if (park.Phone != 0) {
+        details.phoneNumberP.innerText = "Phone Number: " + park.Phone;
+        parentDiv.appendChild(details.phoneNumberP);
+    }
+    
+    if (park.Fax != 0) {
+        details.faxNumberP.innerText = "Fax Number: " + park.Fax;
+        parentDiv.appendChild(details.faxNumberP);
+    }
+
+    if (park.Visit != undefined) {
+        details.visitP.innerText = "Visit: ";
+        details.visitA.innerText = park.Visit;
+        details.visitA.href = park.Visit;
+        details.visitA.target = "_blank"
+        details.visitP.appendChild(details.visitA)
+        parentDiv.appendChild(details.visitP);
+    }
 }
