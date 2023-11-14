33545dfca49cf0c52ec4c8fb10a423d27a950348
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-14 13:44:34 -0700

    Updated the functions and styles

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -23,55 +23,107 @@ function displayPark(park, parentContainer) {
 }
 
 function displayDetails(park, parentDiv) {
-    let details = {};
-    details.parkNameH4 = document.createElement("h4");
-    details.addressP = document.createElement("p");
-    details.phoneNumberP = document.createElement("p");
-    details.faxNumberP = document.createElement("p");
-    details.visitP = document.createElement("p");
-        details.visitA = document.createElement("a");
-        details.visitP.appendChild(details.visitA);
+    const parkNameH4 = document.createElement("h4");
+    parkNameH4.innerText = park.LocationName;
+    parentDiv.appendChild(parkNameH4);
 
-    details.parkNameH4.innerText = park.LocationName;
-    parentDiv.appendChild(details.parkNameH4);
-    
-    doesValueExists(details, park, parentDiv);
-    
-    
-    
-    
-    
-    
+    getAddress(park, parentDiv);
+    getPhoneFaxNumber(park, parentDiv);
+    getVisit(park, parentDiv);
+
+    const dividerHr = document.createElement("hr");
+    dividerHr.classList.add("hrJS");
+    parentDiv.appendChild(dividerHr);
 }
 
-function doesValueExists(details, park, parentDiv) {
-    let parkAddress = {};
-    details.addressP.innerText = "Address: ";
-    for(const key in park) {
-        if(key == "Address" || key == "City")
-        {
-            if(park[key] != 0) details.addressP.innerText += `${park[key]}, `;
+function getAddress(park, parentDiv) {  
+    const addressP = document.createElement("p"); 
+
+        addressP.innerText = "Address: ";
+        for(const key in park) {
+            if(key == "Address" || key == "City")
+            {
+                if(park[key] != 0) addressP.innerText += `${park[key]}, `;
+            }
+            else if (key == "State" || key == "ZipCode") addressP.innerText += `${park[key]} `;
         }
-        else if (key == "State" || key == "ZipCode") details.addressP.innerText += `${park[key]} `;
-    }
-    parentDiv.appendChild(details.addressP);
-    
-    if (park.Phone != 0) {
-        details.phoneNumberP.innerText = "Phone Number: " + park.Phone;
-        parentDiv.appendChild(details.phoneNumberP);
-    }
-    
-    if (park.Fax != 0) {
-        details.faxNumberP.innerText = "Fax Number: " + park.Fax;
-        parentDiv.appendChild(details.faxNumberP);
-    }
 
-    if (park.Visit != undefined) {
-        details.visitP.innerText = "Visit: ";
-        details.visitA.innerText = park.Visit;
-        details.visitA.href = park.Visit;
-        details.visitA.target = "_blank"
-        details.visitP.appendChild(details.visitA)
-        parentDiv.appendChild(details.visitP);
-    }
+    parentDiv.appendChild(addressP);
 }
+
+function getPhoneFaxNumber(park, parentDiv) {
+    const phoneNumberP = document.createElement("p");
+        if (park.Phone != 0) {
+            phoneNumberP.innerText = "Phone Number: " + park.Phone;
+        }
+    parentDiv.appendChild(phoneNumberP);
+
+    const faxNumberP = document.createElement("p");
+        if (park.Fax != 0) {
+            faxNumberP.innerText = "Fax Number: " + park.Fax;
+            parentDiv.appendChild(faxNumberP);
+        }
+    parentDiv.appendChild(faxNumberP);
+}
+
+function getVisit(park, parentDiv) {
+    const visitP = document.createElement("p");
+        const visitA = document.createElement("a");
+
+            if (park.Visit != undefined) {
+                visitP.innerText = "Visit: ";
+                visitA.innerText = park.Visit;
+                visitA.href = park.Visit;
+                visitA.target = "_blank"
+                
+            }
+
+        visitP.appendChild(visitA)
+    parentDiv.appendChild(visitP);
+}
+
+
+// function doesValueExists(details, park, parentDiv) {
+//     details.addressP.innerText = "Address: ";
+//     for(const key in park) {
+//         if(key == "Address" || key == "City")
+//         {
+//             if(park[key] != 0) details.addressP.innerText += `${park[key]}, `;
+//         }
+//         else if (key == "State" || key == "ZipCode") details.addressP.innerText += `${park[key]} `;
+//     }
+//     parentDiv.appendChild(details.addressP);
+    
+//     if (park.Phone != 0) {
+//         details.phoneNumberP.innerText = "Phone Number: " + park.Phone;
+//         parentDiv.appendChild(details.phoneNumberP);
+//     }
+    
+//     if (park.Fax != 0) {
+//         details.faxNumberP.innerText = "Fax Number: " + park.Fax;
+//         parentDiv.appendChild(details.faxNumberP);
+//     }
+
+//     if (park.Visit != undefined) {
+//         details.visitP.innerText = "Visit: ";
+//         details.visitA.innerText = park.Visit;
+//         details.visitA.href = park.Visit;
+//         details.visitA.target = "_blank"
+//         details.visitP.appendChild(details.visitA)
+//         parentDiv.appendChild(details.visitP);
+//     }
+
+
+    // let details = {};
+    // details.parkNameH4 = document.createElement("h4");
+    // details.addressP = document.createElement("p");
+    // details.phoneNumberP = document.createElement("p");
+    // details.faxNumberP = document.createElement("p");
+    // details.visitP = document.createElement("p");
+    //     details.visitA = document.createElement("a");
+
+    // details.parkNameH4.innerText = park.LocationName;
+    // parentDiv.appendChild(details.parkNameH4);
+    
+    // doesValueExists(details, park, parentDiv);  
+// }
