0010e31f5709744c77e76bbc58cb6b09eae257e0
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-16 12:02:43 -0700

    Added functions for displaying data

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     addData();
+    displayMountains(mountainsArray);
 });
 
 function getInput() {
@@ -51,3 +52,77 @@ function addData() {
         }
     }
 }
+
+function displayMountains(mountains) {
+    const input = getInput();
+    const mainContainer = document.getElementById("display-mountains-content");
+    mainContainer.innerText = "";
+
+    mountains.forEach(mountain => {
+        displayMountain(mountain, mainContainer);
+    });
+
+    const results = document.getElementById("results");
+    results.innerText = mountains.length;
+
+    // if(mountains.length == 0) {
+    //     const alertMessage = document.getElementById("alert-message");
+    //     alertMessage.hidden = false;
+    //     const state = document.getElementById("state");
+    //     state.innerText = input.location.value;
+
+    // }
+}
+
+function displayMountain(mountain, parentContainer) {
+    const eachMountainContainerDiv = document.createElement("div");
+    eachMountainContainerDiv.classList.add("each-mountain");
+    parentContainer.appendChild(eachMountainContainerDiv);
+
+    displayDetails(mountain, eachMountainContainerDiv);
+}
+
+    function displayDetails(mountain, parentDiv) {
+        const mountainNameH4 = document.createElement("h4");
+        mountainNameH4.innerText = mountain.name;
+        parentDiv.appendChild(mountainNameH4);
+
+        getElevation(mountain, parentDiv);
+        getEffortLevel(mountain, parentDiv);
+        getDescription(mountain, parentDiv);
+       
+
+        const dividerHr = document.createElement("hr");
+        dividerHr.classList.add("hrJS");
+        parentDiv.appendChild(dividerHr);
+    }
+
+    function getElevation(mountain, parentDiv) {
+        const elevationP = document.createElement("p");
+            const elevationSpan = document.createElement("span");
+            elevationSpan.innerText = "Elevation: ";
+            elevationSpan.classList.add("fw-medium");
+            elevationP.appendChild(elevationSpan);
+            elevationSpan.insertAdjacentText("afterend", mountain.elevation);
+        parentDiv.appendChild(elevationP);
+    }
+
+    function getEffortLevel(mountain, parentDiv) {
+        const effortLevelP = document.createElement("p");
+            const effortLevelSpan = document.createElement("span");
+            effortLevelSpan.innerText = "Effort Level: ";
+            effortLevelSpan.classList.add("fw-medium");
+            effortLevelP.appendChild(effortLevelSpan);
+            effortLevelSpan.insertAdjacentText("afterend", mountain.effort);
+        parentDiv.appendChild(effortLevelP);
+    }
+
+    function getDescription(mountain, parentDiv) {
+        const descriptionP = document.createElement("p");
+            const descriptionSpan = document.createElement("span");
+            descriptionSpan.innerText = "Description: ";
+            descriptionSpan.classList.add("fw-medium");
+            descriptionP.appendChild(descriptionSpan);
+            descriptionSpan.insertAdjacentText("afterend", mountain.desc);
+        parentDiv.appendChild(descriptionP);
+    }
